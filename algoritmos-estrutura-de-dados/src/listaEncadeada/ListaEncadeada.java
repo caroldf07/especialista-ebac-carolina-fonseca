c659bd81ed8e45626d2374ce35fed6006650305d
caroldf07 <carolina.df@outlook.com>
2023-02-01 19:05:06 -0300

    mod01 - lista - 50%.

--- algoritmos-estrutura-de-dados/src/listaEncadeada/ListaEncadeada.java
+++ algoritmos-estrutura-de-dados/src/listaEncadeada/ListaEncadeada.java
@@ -1,4 +1,59 @@
 package listaEncadeada;
 
 public class ListaEncadeada {
+
+    private static class NO {
+        private int dado;
+        private NO proximo;
+    }
+
+    private NO inicio;
+    private NO fim;
+    private int size;
+
+    public void push(int novoDado) {
+        NO novo = new NO();
+        novo.dado = novoDado;
+        if (inicio == null) {
+            inicio = novo;
+            fim = novo;
+            novo.proximo = null;
+        } else {
+            fim.proximo = novo;
+            fim = novo;
+            fim.proximo = null;
+        }
+        size++;
+    }
+
+    public boolean isEmpty() {
+        return inicio == null;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public void remove(int posicao) {
+        if (inicio == null || this.size < posicao) {
+            throw new IndexOutOfBoundsException("O tamanho da lista é menor do que a posição " +
+                    "informada ou a lista está vazia.");
+        } else {
+            NO auxiliar = inicio;
+
+            if (posicao == 0) {
+                inicio = auxiliar.proximo;
+                size--;
+                return;
+            }
+
+            for (int i = 0; auxiliar != null && i < posicao - 1; i++) {
+                auxiliar = auxiliar.proximo;
+            }
+
+            NO proximo = auxiliar.proximo.proximo;
+            auxiliar.proximo = proximo;
+            size--;
+        }
+    }
 }
---
+++ algoritmos-estrutura-de-dados/src/listaEncadeada/Main.java
@@ -0,0 +1,69 @@
+package listaEncadeada;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaEncadeada lista = new ListaEncadeada();
+        Scanner entrada = new Scanner(System.in);
+        int opcao;
+        int input;
+        String LISTA_VAZIA = "A lista está vazia";
+        do {
+            System.out.println("Escolha uma opção");
+            System.out.println("1 - Inserir item na lista");
+            System.out.println("2 - Consultar tamanho da lista");
+            System.out.println("3 - Inserir item na lista em determinada posição");
+            System.out.println("4 - Remover item da lista");
+            System.out.println("5 - Remover item da lista em determinada posição");
+            System.out.println("6 - Consultar item em determinada posição na lista");
+            System.out.println("7 - Exibir lista completa");
+            System.out.println("0 - Sair");
+
+            opcao = entrada.nextInt();
+
+            if (opcao < 0 || opcao > 7) {
+                System.out.println("Opção inválida, tente novamente");
+            }
+
+            if (opcao == 1) {
+                System.out.println("Digite valor inteiro para o dado a ser inserido: ");
+                input = entrada.nextInt();
+                lista.push(input);
+                System.out.println("Número inserido com sucesso");
+            }
+
+            if (opcao == 2) {
+                if (lista.isEmpty()) System.out.println(LISTA_VAZIA);
+                else System.out.println("O tamanho da lista é: " + lista.size());
+            }
+
+  /*           if (opcao == 3) {
+                if (lista.isEmpty()) System.out.println(LISTA_VAZIA);
+                else {
+                    System.out.println("Digite valor inteiro para o dado a ser inserido: ");
+                    input = entrada.nextInt();
+                    System.out.println("Digite a posição que deseja inseri-lo: ");
+                    lista.insert(input, entrada.nextInt());
+                    System.out.println("Número inserido com sucesso");
+                }
+            }
+            if (opcao == 4) {
+                if (fila.isEmpty()) System.out.println(FILA_VAZIA);
+                else {
+                    System.out.println("O item no início da fila é: " + fila.front());
+                }
+            }*/
+
+            if (opcao == 5) {
+                if (lista.isEmpty()) System.out.println(LISTA_VAZIA);
+                else {
+                    System.out.println("Digite a posição do elemento que deseja remover: ");
+                    lista.remove(entrada.nextInt());
+                    System.out.println("Item removido com sucesso");
+                }
+            }
+        } while (opcao != 0);
+
+    }
+}
